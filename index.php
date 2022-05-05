dc0d406238f6e98e791cca0bfa29bd8581f93849
Santoni92 <98034624+Santoni92@users.noreply.github.com>
2022-05-05 15:44:53 +0200

    creato ed importato file contenente struttura dati

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+//phpinfo();
+
+//importo il file contenente la struttura dati
+include 'strutturaDati.php';
+/*
+foreach($albums as $album)
+{
+    echo('<div>');
+    echo($album["author"]);
+}*/
+?>
