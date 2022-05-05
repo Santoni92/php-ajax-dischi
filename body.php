4b11bfffe0bddd6397191aab9389894160a54998
Santoni92 <98034624+Santoni92@users.noreply.github.com>
2022-05-05 16:50:39 +0200

    aggiunto header

--- body.php
+++ body.php
@@ -1,17 +1,8 @@
 <body>
+    <?php
+        include (__DIR__ . '/header.php');
+    ?>
     <div class="container">
-        <!--?php
-        foreach($albums as $album)
-        {
-            echo('<div class="card">');
-            echo('<img src=' . $album["poster"] . ' >');
-            echo($album["title"]);
-            echo($album["author"]);
-            echo($album["year"]);
-            echo($album["genre"]);
-            echo('</div>');
-        }
-        ?-->
         <?php
             include (__DIR__ . '/card.php'); 
         ?>
