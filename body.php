eaf4dfb5ba0032e293ebf4783d65df85b2d7d8a5
Santoni92 <98034624+Santoni92@users.noreply.github.com>
2022-05-05 17:15:35 +0200

    agganciato vue all'html

--- body.php
+++ body.php
@@ -2,11 +2,22 @@
     <?php
         include (__DIR__ . '/header.php');
     ?>
-    <div class="container">
-        <?php
+    <!-- div class="container">
+        < ?php
             include (__DIR__ . '/card.php'); 
-        ?>
+        ? >
+
+    </div -->
+
+     <!--Seconda Milestone: Attraverso l’utilizzo di axios: al caricamento della pagina axios chiederà, attraverso una chiamata api, i dischi a php e li stamperà attraverso vue.--> 
+    <div id="app">
+        <div class="container">
+            <div v-for="album">
+
+            </div>
+        </div>
     </div>
+
 
     <!--collego il mio script js -->
     <script src="script.js"></script>
